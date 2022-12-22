efe5029a154b034927c6bfcfbfad10c67de1eb60
IgorSotnyk <richyigorek@gmail.com>
2022-12-22 23:45:29 +0200

    add Dockerfile and DB default

--- acount/acount/settings.py
+++ acount/acount/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'acount.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'todo',
-        'USER': 'root',
-        'PASSWORD': '314p618f',
-        'HOST': 'localhost',
-        'PORT': 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
